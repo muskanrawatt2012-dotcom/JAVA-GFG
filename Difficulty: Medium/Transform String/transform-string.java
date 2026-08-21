ab55813bdc0ec668e61170bee224519218451cfa
muskanrawatt2012-dotcom <muskan.rawatt2012@gmail.com>
2026-08-21 22:02:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Transform String/transform-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    int transform(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return -1;
+        }
+
+        int[] count = new int[256];
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1.charAt(i)]++;
+            count[s2.charAt(i)]--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return -1;
+            }
+        }
+
+        int res = 0;
+        int i = s1.length() - 1;
+        int j = s2.length() - 1;
+
+        while (i >= 0) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                j--;
+            } else {
+                res++;
+            }
+            i--;
+        }
+
+        return res;
+    }
+}
