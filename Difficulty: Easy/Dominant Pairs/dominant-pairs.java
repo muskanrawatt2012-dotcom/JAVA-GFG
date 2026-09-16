5b80e8ec45328c1bc9cb417a128ce656cbda2f48
muskanrawatt2012-dotcom <muskan.rawatt2012@gmail.com>
2026-09-16 20:25:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Dominant Pairs/dominant-pairs.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int dominantPairs(int[] arr) {
+        int n = arr.length;
+        int mid = n / 2;
+
+        Arrays.sort(arr, 0, mid);
+        Arrays.sort(arr, mid, n);
+
+        int count = 0;
+        int j = mid;
+
+        for (int i = 0; i < mid; i++) {
+            while (j < n && arr[i] >= 5 * arr[j]) {
+                j++;
+            }
+            count += (j - mid);
+        }
+
+        return count;
+    }
+}
